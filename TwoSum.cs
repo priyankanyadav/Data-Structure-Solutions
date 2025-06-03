c0f3b62cd26394f8cb3888f08358529b4e6c5fe9
Priyanka Yadav <priyankanyadav1144@gmail.com>
2025-06-03 16:17:59 -0400

    Create TwoSum.cs

---
+++ TwoSum.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        
+        for (int i = 0; i < nums.Length; i++) {
+            int cur = nums[i];
+            int x = target - cur;
+            
+            if (map.ContainsKey(x)) {
+                return new int[] { map[x], i };
+            }
+
+            if (!map.ContainsKey(cur)) {
+                map[cur] = i;
+            }
+        }
+
+        return new int[] {}; // LeetCode says there is always one solution
+    }
+}
